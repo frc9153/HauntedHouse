ff7ac00a0f9d516ddce79fb9d28f649b37ea17c1
somebody <onesomegit@outlook.com>
2023-08-24 18:30:10 -0500

    Work

--- alg/index.js
+++ alg/index.js
@@ -8,6 +8,7 @@ const CONFIG = {
     ],
     SATISFACTORY_DISTANCE: 5,
     MAX_SPEED: 5,
+    ROT_SPEED: 4
 };
 
 /** @type {HTMLCanvasElement} */
@@ -133,6 +134,33 @@ class Vector2 {
     }
 }
 
+class Rect {
+    constructor(position, size) {
+        this.position = position;
+        position.sanityCheck();
+
+        this.size = size;
+        size.sanityCheck();
+    }
+
+    expandedFromCenter(amount) {
+        return new Rect(
+            this.position.minus(amount / 2),
+            this.size.plus(amount)
+        );
+    }
+
+    containsPoint(point) {
+        if (point.x < this.position.x) return false;
+        if (point.y < this.position.y) return false;
+
+        if (point.x > this.size.x + this.position.x) return false;
+        if (point.y > this.size.y + this.position.y) return false;
+
+        return true;
+    }
+}
+
 class Robot {
     constructor(sizeFt) {
         this.sizeFt = Vector2.fromArray(sizeFt);
@@ -142,32 +170,87 @@ class Robot {
             Math.round(this.sizeFt.y * pixelsPerFoot[1]),
         );
 
-        this.positionPx = new Vector2(50, 50);
-        this.rotationDeg = 37;
+        this.positionPx = new Vector2(150, 150);
+        this.rotationDeg = 0;
+        this.targetRotation = 0;
 
         this.targetPos = null;
+        this.targetPath = [];
     }
 
     render() {
         this.positionPx.sanityCheck();
         this.sizePx.sanityCheck();
 
-        // Draw circle target (global position)
+        // Draw target (global position)
         if (this.targetPos) {
-            ctx.strokeStyle = "red";
+            ctx.strokeStyle = "yellow";
             ctx.lineWidth = 2;
             ctx.beginPath();
-            ctx.arc(
-                ...this.targetPos.toArray(),
-                5,
-                0,
-                2 * Math.PI,
-                false
-            );
+            ctx.moveTo(...this.positionPx.toArray());
+            ctx.lineTo(...this.targetPos.toArray());
             ctx.stroke();
         }
 
-        // State restore trick for rotating from https://stackoverflow.com/a/11985464
+        // Bezier curve test
+        if (this.targetPos) {
+            ctx.beginPath();
+            ctx.strokeStyle = "yellow";
+            // const pos1 = new Vector2(0, 0);
+            // const pos2 = new Vector2(200, 0);
+            // const pos3 = new Vector2(200, 400);
+            // const pos4 = new Vector2(400, 400);
+
+            const pos1 = this.positionPx;
+            const pos2 = new Vector2(
+                (this.positionPx.x + this.targetPos.x) / 2, this.positionPx.y
+            );
+            const pos3 = new Vector2(
+                (this.positionPx.x + this.targetPos.x) / 2, this.targetPos.y
+            );
+            const pos4 = this.targetPos;
+
+            ctx.moveTo(...pos1.toArray());
+            for (let t = 0; t < 1; t += 0.05) {
+                const x = (Math.pow(1 - t, 3) * pos1.x) +
+                    (3 * Math.pow(1 - t, 2) * t * pos2.x) +
+                    (3 * (1 - t) * Math.pow(t, 2) * pos3.x) +
+                    (Math.pow(t, 3) * pos4.x);
+
+                const y = (Math.pow(1 - t, 3) * pos1.y) +
+                    (3 * Math.pow(1 - t, 2) * t * pos2.y) +
+                    (3 * (1 - t) * Math.pow(t, 2) * pos3.y) +
+                    (Math.pow(t, 3) * pos4.y);
+
+
+                ctx.lineTo(x, y);
+            }
+            ctx.stroke();
+        }
+
+        // Target path
+        if (this.targetPath.length) {
+            ctx.beginPath();
+            ctx.strokeStyle = "lime";
+            ctx.moveTo(...this.positionPx.toArray());
+            for (const pos of this.targetPath) {
+                ctx.lineTo(...aStarGrid.localToGlobal(pos).toArray());
+            }
+            ctx.stroke();
+        }
+
+        // Show target rotation
+        ctx.save();
+        ctx.translate(...this.positionPx.toArray());
+        ctx.rotate(this.targetRotation * Math.PI / 180);
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(0, this.sizePx.x / 2);
+        ctx.lineTo(0, 100);
+        ctx.stroke();
+        ctx.restore();
+
         ctx.save();
 
         // put center of bot at origin
@@ -202,6 +285,7 @@ class Robot {
 
     postRenderProcess() {
         this.creepCloserToTarget();
+        this.turnABit();
         updateDebug("Robot.sizeFt", this.sizeFt.toString());
         updateDebug("Robot.sizePx", this.sizePx.toString());
         updateDebug("Robot.rotationDeg", this.rotationDeg);
@@ -215,30 +299,77 @@ class Robot {
         return false;
     }
 
+    updateCurrentTarget() {
+        if (!this.targetPath.length) return;
+        this.targetPos = aStarGrid.localToGlobal(this.targetPath.shift());
+        // this.targetRotation = Math.random() * 360;
+        aStarGrid.targetPos = this.nodePos;
+    }
+
     creepCloserToTarget() {
-        if (this.closeEnoughToTarget()) return;
+        if (this.closeEnoughToTarget()) {
+            this.updateCurrentTarget();
+            return;
+        }
 
         // get normalized direction vector
         const deltaVector = this.targetPos.minus(this.positionPx);
         const direction = deltaVector.div(deltaVector.magnitude());
         this.positionPx = this.positionPx.plus(direction.mult(CONFIG.MAX_SPEED));
 
+        this.targetRotation = (Math.atan2(direction.y, direction.x) * 180 / Math.PI) - 90;
+
         if (this.closeEnoughToTarget()) {
-            this.targetPos = null;
+            this.updateCurrentTarget();
         }
     }
 
+    turnABit() {
+        const rotError = this.targetRotation - this.rotationDeg;
+
+        if (Math.abs(rotError) < CONFIG.ROT_SPEED) {
+            // Too small of a turn, just set it
+            this.rotationDeg = this.targetRotation;
+            return;
+        }
+
+        // No div by zero pls!
+        let rotErrorSign = 1;
+        if (rotError !== 0) rotErrorSign = rotError / Math.abs(rotError);
+
+        this.rotationDeg += CONFIG.ROT_SPEED * rotErrorSign;
+    }
+
     gotoPos(posPx) {
-        this.targetPos = Vector2.fromArray(posPx);
-
-
         const nodePos = aStarGrid.getNodePosFromPx(posPx);
         const botPos = aStarGrid.getNodePosFromPx(this.positionPx.toArray());
 
-        aStarGrid.targetPos = nodePos;
-        let path = aStarGrid.search(botPos, nodePos);
-        aStarGrid.highlightedPositions = path;
-        console.log(path);
+        this.targetPath = aStarGrid.search(botPos, nodePos);
+        aStarGrid.highlightedPositions = this.targetPath;
+        return;
+
+        // TODO: ALL THAT
+        let lastGood = botPos;
+        let anchorPoints = [];
+        // HACK!!
+        let oldHashes = [];
+        for (const pos of this.targetPath) {
+            let end = aStarGrid.raycastToLastGood(lastGood, pos);
+            if (!end) continue;
+
+            lastGood = end;
+            if (oldHashes.includes(end.toString())) continue;
+            anchorPoints.push(end);
+            oldHashes.push(end.toString());
+        }
+
+        if (!anchorPoints.length) {
+            anchorPoints = [nodePos];
+        }
+
+        this.targetPath = anchorPoints;
+        aStarGrid.highlightedPositions = this.targetPath;
+        console.log(anchorPoints);
     }
 }
 
@@ -256,8 +387,10 @@ canvas.addEventListener("mousemove", function (e) {
 // Bad zones
 class BadZone {
     constructor(positionPx, sizePx) {
-        this.positionPx = Vector2.fromArray(positionPx);
-        this.sizePx = Vector2.fromArray(sizePx);
+        this.rect = new Rect(
+            Vector2.fromArray(positionPx),
+            Vector2.fromArray(sizePx)
+        );
     }
 
     render() {
@@ -267,12 +400,12 @@ class BadZone {
         ctx.fillStyle = "#ffca2b93";
 
         ctx.fillRect(
-            ...this.positionPx.toArray(),
-            ...this.sizePx.toArray(),
+            ...this.rect.position.toArray(),
+            ...this.rect.size.toArray(),
         );
         ctx.strokeRect(
-            ...this.positionPx.toArray(),
-            ...this.sizePx.toArray(),
+            ...this.rect.position.toArray(),
+            ...this.rect.size.toArray(),
         );
         ctx.setLineDash([]);
 
@@ -281,9 +414,9 @@ class BadZone {
         for (const [lineNo, text] of Object.entries("EVIL ZONE!\nDONT GO HERE!!!!\nBAD!! KILL!!!!".split("\n"))) {
             ctx.fillText(
                 text,
-                this.positionPx.x,
-                this.positionPx.y + (50 * (Number(lineNo) + 1)),
-                this.sizePx.x
+                this.rect.position.x,
+                this.rect.position.y + (50 * (Number(lineNo) + 1)),
+                this.rect.size.x
             );
         }
     }
@@ -308,7 +441,7 @@ class AStarNode {
         this.f = null;
 
         this.parent = null;
-
+        this.okay = true;
         // FIXME: HACK!!!!!!!!!!!!
         this.pos = pos;
     }
@@ -326,6 +459,72 @@ class AStarGrid {
 
         this.targetPos = null;
         this.highlightedPositions = [];
+        this.rayPos = [];
+
+        this.decayingRenderElement = [
+        ];
+    }
+
+    raycastToLastGood(pos1, pos2) {
+        // get a list of points between the points with bresenham's algorithm
+        // C# implementation from
+        // https://www.codeproject.com/Articles/15604/Ray-casting-in-a-2D-tile-based-environment
+
+        let steep = Math.abs(pos2.y - pos1.y) > Math.abs(pos2.x - pos1.x);
+
+        if (steep) {
+            pos1 = new Vector2(pos1.y, pos1.x);
+            pos2 = new Vector2(pos2.y, pos2.x);
+        }
+
+        if (pos1.x > pos2.x) {
+            let oneBuf = pos1.toArray();
+            pos1 = new Vector2(pos2.x, pos2.y);
+            pos2 = Vector2.fromArray(oneBuf);
+        }
+
+        let delta = new Vector2(
+            pos2.x - pos1.x,
+            Math.abs(pos2.y - pos1.y)
+        );
+        let error = 0;
+        let y = pos1.y;
+        let yStep = (pos1.y < pos2.y) ? 1 : -1;
+
+        let last = null;
+        this.rayPos = [];
+        for (let x = pos1.x; x <= pos2.x; x++) {
+            let vec = steep ? new Vector2(y, x) : new Vector2(x, y);
+
+            // if (last) {
+            //     const oThis = this;
+            //     this.decayingRenderElement.push(
+            //         {
+            //             framesLeft: 20, func: function (context) {
+            //                 context.beginPath();
+            //                 context.strokeStyle = "cyan";
+            //                 context.moveTo(...oThis.localToGlobal(last).toArray());
+            //                 context.lineTo(...oThis.localToGlobal(vec).toArray());
+            //                 context.stroke();
+            //             }
+            //         }
+            //     );
+            // }
+
+            if (!this.nodes[vec.x][vec.y].okay) {
+                this.rayPos = [last];
+                return last;
+            }
+
+            last = vec;
+            this.rayPos.push(vec);
+            error += delta.y;
+            if (2 * error >= delta.x) {
+                y += yStep;
+                error -= delta.x;
+            }
+        }
+        return null;
     }
 
     resetNodes() {
@@ -336,9 +535,21 @@ class AStarGrid {
             () => Array.from({ length: this.resolution.y })//, () => new AStarNode())
         );
 
-        for (let x=0;x<this.nodes.length;x++) {
-            for (let y=0;y<this.nodes[x].length;y++) {
-                this.nodes[x][y] = new AStarNode([x, y]);
+        for (let x = 0; x < this.nodes.length; x++) {
+            for (let y = 0; y < this.nodes[x].length; y++) {
+                let node = new AStarNode([x, y]);
+                let globalPos = new Vector2(
+                    (x * this.nodeSize.x) + this.offset.x + (this.nodeSize.x / 2),
+                    (y * this.nodeSize.y) + this.offset.y
+                );
+
+                for (const badZone of badZones) {
+                    if (!badZone.rect.expandedFromCenter(100).containsPoint(globalPos)) continue;
+                    node.okay = false;
+                    break;
+                }
+
+                this.nodes[x][y] = node;
             }
         }
     }
@@ -392,10 +603,12 @@ class AStarGrid {
                 // old news GRANDPA!!!!!!!!!!
                 if (closedHashes.includes(neighborPos.toString())) continue;
 
+                let neighborNode = this.nodes[neighborPos.x][neighborPos.y];
+                if (!neighborNode.okay) continue;
+
                 let gScore = currentNode.g + 1;
                 let gScoreBest = false;
 
-                let neighborNode = this.nodes[neighborPos.x][neighborPos.y];
                 if (neighborNode.f === null) {
                     // New!
                     gScoreBest = true;
@@ -417,15 +630,19 @@ class AStarGrid {
         return [];
     }
 
+    localToGlobal(localPos) {
+        return localPos.mult(this.nodeSize).plus(this.offset).plus(this.nodeSize.div(2));
+    }
+
     render() {
         for (let y = 0; y < this.resolution.y; y++) {
             for (let x = 0; x < this.resolution.x; x++) {
                 let vec = new Vector2(x, y);
+                let node = this.nodes[x][y];
                 let styled = false;
 
                 for (const hlPos of this.highlightedPositions) {
                     if (!hlPos.equals(vec)) continue;
-                    console.log("OK")
                     styled = true;
 
                     ctx.lineWidth = 4;
@@ -433,12 +650,31 @@ class AStarGrid {
                 }
 
                 if (!styled) {
+                    for (const ray of this.rayPos) {
+                        if (!ray.equals(vec)) continue;
+                        styled = true;
+
+                        ctx.lineWidth = 4;
+                        ctx.strokeStyle = "pink";
+                    }
+                }
+
+                if (!styled) {
                     if (this.targetPos && this.targetPos.equals(vec)) {
                         ctx.lineWidth = 4;
                         ctx.strokeStyle = "aqua";
+                    } else if (node.okay === false) {
+                        ctx.fillStyle = "#0000ff33";
+                        ctx.fillRect(
+                            (x * this.nodeSize.x) + this.offset.x,
+                            (y * this.nodeSize.y) + this.offset.y,
+                            this.nodeSize.x,
+                            this.nodeSize.y,
+                        );
+                        continue;
                     } else {
                         ctx.lineWidth = 1;
-                        ctx.strokeStyle = "red";
+                        ctx.strokeStyle = "#ff000033";
                     }
                 }
 
@@ -450,18 +686,29 @@ class AStarGrid {
                 );
             }
         }
+
+        // decayables
+        for (const re of this.decayingRenderElement) {
+            re.framesLeft--;
+            if (re.framesLeft < 0) {
+                this.decayingRenderElement = this.decayingRenderElement.filter(r => r !== re);
+            }
+
+            re.func(ctx);
+        }
     }
 
     getNodePosFromPx(posPx) {
         let vec = Vector2.fromArray(posPx);
         return new Vector2(
-            (vec.x - this.offset.x) / this.nodeSize.x,
-            (vec.y - this.offset.y) / this.nodeSize.y
+            (vec.x - this.offset.x) / this.nodeSize.x - 0.5,
+            (vec.y - this.offset.y) / this.nodeSize.y - 0.5
         ).round();
     }
 }
 
-const aStarGrid = new AStarGrid(fieldData["field-corners"]["top-left"], fieldSizePx, [20, 12]);
+// const aStarGrid = new AStarGrid(fieldData["field-corners"]["top-left"], fieldSizePx, [20, 12]);
+const aStarGrid = new AStarGrid(fieldData["field-corners"]["top-left"], fieldSizePx, [40, 24]);
 updateDebug("AStarGrid.resolution", aStarGrid.resolution);
 updateDebug("AStarGrid.nodeCount", aStarGrid.resolution.x * aStarGrid.resolution.y);
 
