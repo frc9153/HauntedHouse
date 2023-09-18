94d8b9740ef39bf97b94cb67fdcb52c5f8f535c3
somebody <onesomegit@outlook.com>
2023-09-18 16:28:44 -0500

    Get ready to test

---
+++ nwtables_experiment/dump_changes.py
@@ -0,0 +1,15 @@
+import time
+from typing import Any
+from networktables import NetworkTables
+
+NetworkTables.initalize(server="10.91.53.2")
+
+def on_value_changed(key: str, value: Any, is_new: bool) -> None:
+    prefix = "[NEW]" if is_new else ""
+    print(f"{prefix} '{key}': {value}")
+
+NetworkTables.addEntryListener(on_value_changed)
+
+# Keep main thread busy for a while (he doesn't know)
+while True:
+    time.sleep(1)
