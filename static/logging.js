d4714a69efc064dc8a851cf285b07e05589f0df7
somebody <onesomegit@outlook.com>
2024-01-23 18:53:27 -0600

    Update version

--- static/logging.js
+++ static/logging.js
@@ -7,7 +7,7 @@ function updateDebug(k, v) {
     if (v === k[v]) return;
 
     debugKV[k] = v;
-    let texts = ["[HauntedHouse A.0.1 - GISELLE-1]"];
+    let texts = ["[HauntedHouse A.0.2 - GISELLE-1]"];
     for (const [k, v] of Object.entries(debugKV)) {
         texts.push(`${k}: ${v}`);
     }
